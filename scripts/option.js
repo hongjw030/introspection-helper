d37fd76922d6962a6976a46a67710e09830e153c
hongjw030 <hongjw030@naver.com>
2024-06-07 08:29:03 +0900

    feat: 커밋메세지 설정 기능 추가

--- scripts/option.js
+++ scripts/option.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-  chrome.storage.local.get(['habit', 'savedTemplate', 'isLight'], async function(result) {
+  chrome.storage.local.get(null, async function(result) {
     if (result.habit){
       document.getElementById('option-habit-checkbox').setAttribute('checked', true);
     }
     if (result.savedTemplate) {
       document.getElementById('option-template-textarea').value = result.savedTemplate;
+    }
+    if(result.savedCommit){
+      document.getElementById('option-commit-input').value = result.savedCommit;
+    }
+    if(!result.savedCommit){
+      document.getElementById('option-commit-input').value = 'Create new Markdown file'
     }
     if(!result.isLight || result.isLight==='yes'){
       const mainBody = document.getElementById('extension-body');
@@ -49,6 +55,26 @@ document.addEventListener('DOMContentLoaded', function() {
     }else{
       chrome.storage.local.set({"habit": false});
     }
+  })
+
+  // 커밋 저장 버튼 기능
+  document.getElementById('option-commit-save-button').addEventListener('click', function(){
+    const commitInput = document.getElementById('option-commit-input');
+    if (!commitInput.value || commitInput.value == ''){
+      alert('커밋 메세지는 반드시 있어야 합니다.');
+    }
+    else{
+      chrome.storage.local.set({savedCommit: commitInput.value});
+      alert("커밋 메세지가 저장되었습니다!");  
+    }
+    return;
+  })
+
+  //커밋 리셋 버튼 기능
+  document.getElementById('option-commit-reset-button').addEventListener('click', function(){
+    chrome.storage.local.remove(['savedCommit']);
+    document.getElementById('option-commit-input').value = 'Create new Markdown file';
+    alert("커밋 메세지를 초기화했습니다!");
   })
 
   // 템플릿 저장 버튼 기능
--- scripts/popup.js
+++ scripts/popup.js
@@ -17,7 +17,7 @@ const SUBMISSION_DATE = `${YEAR}${MONTH}${DAY}`;
 
 const CLIENT_ID = 'Ov23lisuJkODBkIrQm4e';
 const CLIENT_SECRET ='2483bad98853659aade58aa88f50b1a44765775a';
-// chrome.storage.local의 키: ['githubToken', 'selectedRepo', 'nickname', 'savedText', 'savedTemplate', 'habit', 'submissionDate', 'isLight']
+// chrome.storage.local의 키: ['githubToken', 'selectedRepo', 'nickname', 'savedText', 'savedTemplate', 'habit', 'submissionDate', 'isLight', 'savedCommit']
 
 document.addEventListener('DOMContentLoaded', function() {
   const REDIRECT_URI = chrome.identity.getRedirectURL();
