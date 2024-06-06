fb103877e8c734d7a10958777dc1105d62c629ae
hongjw030 <hongjw030@naver.com>
2024-06-06 21:56:33 +0900

    style: 다크모드 textarea 색 변경

--- scripts/option.js
+++ scripts/option.js
@@ -1,13 +1,47 @@
 document.addEventListener('DOMContentLoaded', function() {
-  chrome.storage.local.get(['habit', 'savedTemplate'], async function(result) {
+  chrome.storage.local.get(['habit', 'savedTemplate', 'isLight'], async function(result) {
     if (result.habit){
       document.getElementById('option-habit-checkbox').setAttribute('checked', true);
     }
     if (result.savedTemplate) {
       document.getElementById('option-template-textarea').value = result.savedTemplate;
-    } 
+    }
+    if(!result.isLight || result.isLight==='yes'){
+      const mainBody = document.getElementById('extension-body');
+      const themeButton = document.getElementById('theme-button');
+      const themeImg = document.getElementById('theme-img');
+      themeButton.setAttribute('data-isLight', 'yes');
+      themeImg.setAttribute('src', "../assets/sun.svg");
+      mainBody.setAttribute('class', "")
+      chrome.storage.local.set({'isLight': 'yes'});
+    }else{
+      const mainBody = document.getElementById('extension-body');
+      const themeButton = document.getElementById('theme-button');
+      const themeImg = document.getElementById('theme-img');
+      themeButton.setAttribute('data-isLight', 'no');
+      themeImg.setAttribute('src', "../assets/moon.svg");
+      mainBody.setAttribute('class', "dark-theme")
+      chrome.storage.local.set({'isLight': 'no'});
+    }
   });
-
+  document.getElementById('theme-button').addEventListener('click',()=>{
+    const mainBody = document.getElementById('extension-body');
+    const themeButton = document.getElementById('theme-button');
+    const themeImg = document.getElementById('theme-img');
+    chrome.storage.local.get('isLight', (result)=>{
+      if (result.isLight === 'yes'){
+        themeButton.setAttribute('data-isLight', 'no');
+        themeImg.setAttribute('src', "../assets/moon.svg");
+        mainBody.setAttribute('class', "dark-theme")
+        chrome.storage.local.set({'isLight': 'no'});
+      }else{
+        themeButton.setAttribute('data-isLight', 'yes');
+        themeImg.setAttribute('src', "../assets/sun.svg");
+        mainBody.setAttribute('class', "")
+        chrome.storage.local.set({'isLight': 'yes'});
+      }
+    });
+  })
   // 오늘 회고 작성했는지 여부 확인 기능
   document.getElementById('option-habit-checkbox').addEventListener('change', ({target})=>{
     if (target.checked){
